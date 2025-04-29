85200f8b009b031c3c1d0ba04e18cb8867c24e39
AbrorilHuda <moh.abrorilhuda@gmail.com>
2025-04-29 12:30:56 +0700

    github action

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
-
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/AbrorilHuda.github.io" : "",
+  assetPrefix: isProd ? "/AbrorilHuda.github.io/" : "",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
